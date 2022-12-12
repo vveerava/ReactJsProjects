99edc3b8e189d459696934fd8883f33b2a225dbe
vgiridhar <vgiridhar@cloudera.com>
2022-12-12 14:22:40 +0530

    adding sticky notes project

---
+++ StickyNotes/src/App.js
@@ -0,0 +1,35 @@
+import "./Components/CSS/App.css";
+import Notes from "./Components/NoteComponents/Notes";
+import Header from "./Components/NoteComponents/Header";
+function App() {
+  return (
+    <div className="main">
+      <Header />
+      <Notes />
+    </div>
+  );
+}
+export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ StickyNotes/src/Components/NoteComponents/Notes.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import "../CSS/Note.css";
+import CreateNote from "./CreateNote";
+import Note from "./Note";
+import { v4 as uuid } from "uuid";
+
+function Notes() {
+  //states
+  const [notes, setNotes] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  // get text and store in state
+  const textHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  // add new note to the state array
+  const saveHandler = () => {
+    setNotes((prevState) => [
+      ...prevState,
+      {
+        id: uuid(),
+        text: inputText
+      }
+    ]);
+    //clear the textarea
+    setInputText("");
+  };
+
+  //delete note function
+  const deleteNote = (id) => {
+    const filteredNotes = notes.filter((note) => note.id !== id);
+    setNotes(filteredNotes);
+  };
+
+  //apply the save and get functions using useEffect
+  //get the saved notes and add them to the array
+
+  const data = JSON.parse(localStorage.getItem("Notes_Key"));
+
+   useEffect(() => {
+
+    console.log("---vveerava : useEffect: getItem---",data)
+   // const data = JSON.parse(localStorage.getItem("Notes_Key"));
+    if (data == null) {
+        setNotes([])
+    }else{
+      setNotes(data);
+    }
+  }, []);
+
+  //saving data to local storage
+  useEffect(() => {
+    console.log("---vveerava : useEffect: setItem---")
+
+    localStorage.setItem("Notes_Key", JSON.stringify(notes));
+  }, [notes]);
+
+  return (
+    <div className="notes">
+      {notes.map((note) => (
+        <Note
+          key={note.id}
+          id={note.id}
+          text={note.text}
+          deleteNote={deleteNote}
+        />
+      ))}
+      <CreateNote
+        textHandler={textHandler}
+        saveHandler={saveHandler}
+        inputText={inputText}
+      />
+    </div>
+  );
+}
+
+export default Notes;
